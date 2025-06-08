9dbcf20c88f0bac039ea1b5b7dfc9992af864d93
uktmvic <turakulovd1607@gmail.com>
2025-06-08 09:44:07 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from aiogram import executor
+
+from flask import Flask
 import os
 
+app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return 'Hello from Render!'
+
+# PORT muhit o‘zgaruvchisini olish
+port = int(os.environ.get("PORT", 5000))
+
+# Appni ishga tushirish
+app.run(host='0.0.0.0', port=port)
 
 from loader import dp, db
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
-port = int(os.environ.get("PORT", 5000))
-
-app.run(host='0.0.0.0', port=port)
 
 async def on_startup(dispatcher):
     await set_default_commands(dispatcher)
